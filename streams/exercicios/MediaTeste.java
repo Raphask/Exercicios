6808276dec2c45aecb8b9d097c3e6d50bf986835
Raphask <rapharamos223@gmail.com>
2023-05-16 14:24:05 -0300

    Pasta Metadata

--- streams/exercicios/MediaTeste.java
+++
@@ -1,9 +0,0 @@
-package streams.exercicios;
-
-public class MediaTeste {
-public static void main(String[] args) {
-	
- Media m1 = new Media().media(5.4).media(8.9);
- System.out.println(m1.getValor());
-}
-}
